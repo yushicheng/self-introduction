bc091ffeceb36e85b21eed2294a86af2c8ba6c10
yushicheng <1542874601@qq.com>
2022-08-17 20:17:31 +0800

    temp:临时节点标记 星期三 晚上 8:17:30 八月 17日 2022 修改了 src/pages/index.js 删除了 "static/\344\270\252\344\272\272\347\256\200\345\216\206.docx"

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,30 +7,30 @@ import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
 
 import pdf_file from "@site/static/个人简历.pdf";
-import docx_file from "@site/static/个人简历.docx";
+// import docx_file from "@site/static/个人简历.docx";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div style={{margin:"10px 0px"}} className={styles.buttons}>
-          <Link style={{marginBottom:"20px"}} className="button button--secondary button--lg" to="/docs/自我介绍">
+        <div style={{ margin: "10px 0px" }} className={styles.buttons}>
+          <Link style={{ marginBottom: "20px" }} className="button button--secondary button--lg" to="/docs/自我介绍">
             查看详情 ⏱️
           </Link>
         </div>
         <div className={styles.buttons}>
-          <a style={{margin:"0px 10px"}} className="button button--secondary button--lg" href={pdf_file}>预览并下载PDF简历</a>
-          <a style={{margin:"0px 10px"}} className="button button--secondary button--lg" href={docx_file}>预览并下载Word简历</a>
+          <a style={{ margin: "0px 10px" }} className="button button--secondary button--lg" href={pdf_file}>预览并下载PDF简历</a>
         </div>
       </div>
     </header>
-)}
+  )
+}
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}
@@ -40,4 +40,5 @@ export default function Home() {
         <HomepageFeatures />
       </main>
     </Layout>
-)}
+  )
+}
